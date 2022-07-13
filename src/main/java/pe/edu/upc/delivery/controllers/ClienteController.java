a639d1d9b0583381f4de570f65e5a260b94567b0
Hipolito <u20171a057@upc.edu.pe>
2022-07-13 10:26:46 -0500

    controller grabar

--- src/main/java/pe/edu/upc/delivery/controllers/ClienteController.java
+++ src/main/java/pe/edu/upc/delivery/controllers/ClienteController.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pe.edu.upc.delivery.models.entities.Cliente;
+import pe.edu.upc.delivery.models.entities.Distrito;
 import pe.edu.upc.delivery.services.ClienteService;
+import pe.edu.upc.delivery.services.DistritoService;
 
 @Controller
 @RequestMapping("/clientes")
@@ -16,12 +20,19 @@ public class ClienteController {
 	
 	@Autowired
 	private ClienteService clienteService;
+	@Autowired
+	private DistritoService distritoService;
 	
 	@GetMapping
 	public String inicio(Model model) {
+		Cliente cliente = new Cliente();
+		
 		try {
 			List<Cliente> clientes = clienteService.findAll();
+			List<Distrito> distritos = distritoService.findAll();
 			model.addAttribute("clientes", clientes);
+			model.addAttribute("cliente", cliente);
+			model.addAttribute("distritos", distritos);
 			
 		} catch (Exception e) {
 		
@@ -29,6 +40,19 @@ public class ClienteController {
 			System.err.println(e.getMessage());
 		}
 		return "clientes/inicio";		
+	}
+	
+	@PostMapping("save")
+	public String save(@ModelAttribute("cliente") Cliente cliente) {
+		
+		try {
+			clienteService.save(cliente);
+		} catch (Exception e) {			
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+		
+		return "redirect:/clientes";
 	}
 	
 	
