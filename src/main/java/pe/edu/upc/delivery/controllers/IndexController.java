0a4121cb234d3a321dd0fcbce435e8b080c9d603
Hipolito <u20171a057@upc.edu.pe>
2022-07-26 16:45:11 -0500

    avances

--- src/main/java/pe/edu/upc/delivery/controllers/IndexController.java
+++ src/main/java/pe/edu/upc/delivery/controllers/IndexController.java
@@ -4,16 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import pe.edu.upc.delivery.models.entities.Producto;
 
 @Controller
 @RequestMapping("/")
+@SessionAttributes("producto")
 public class IndexController {
 	
 	@GetMapping
 	public String index(Model model) {
-		String saludo ="Hola programadores";
-		model.addAttribute("saludos", saludo);
-		return "index2";
+		Producto producto = new Producto();
+		model.addAttribute("producto", producto);
+		return "index";
 	}
 	
 	
